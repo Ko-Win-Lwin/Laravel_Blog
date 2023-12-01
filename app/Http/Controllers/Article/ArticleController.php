a7967797e0627646bf85f8dccf351c0705f9d71a
winlwin.mce <winlwin.mce@gmail.com>
2023-12-01 14:22:19 +0630

    Model: models

--- app/Http/Controllers/Article/ArticleController.php
+++ app/Http/Controllers/Article/ArticleController.php
@@ -3,22 +3,13 @@
 namespace App\Http\Controllers\Article;
 
 use App\Http\Controllers\Controller;
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function index() {
-        $data = [
-            [
-                "id" => 1, 
-                "title" => "First Article"
-            ],
-
-            [
-                "id" => 2,
-                "title" => "Second Article"
-            ]
-        ];
+        $data = Article::all();
         return view('articles.index', [
             "articles" => $data
         ]);
